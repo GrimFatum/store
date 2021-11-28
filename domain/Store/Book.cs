d967c741494531be66e5b3e0083927d9f4af0e86
AvtoKot <avtokotvelikov@gmail.com>
2021-11-28 21:22:51 +0700

    Добавить интерфейс репозитория книг

---
+++ domain/Store/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Store
+{
+    /// <summary>
+    /// Сущность книги
+    /// </summary>
+    public class Book
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public int ID { get; }
+
+        /// <summary>
+        /// Название книги
+        /// </summary>
+        public string Title { get; }
+
+
+
+        public Book(int id, string title)
+        {
+            ID = id;
+            Title = title;
+        }
+    }
+}
